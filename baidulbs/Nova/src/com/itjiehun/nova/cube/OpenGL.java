b4ada4b9af492ab923cdf8db96881115762f3fc7
liufeiit <liufei_it@126.com>
2014-06-22 21:33:31 +0800

    ss

--- baidulbs/Nova/src/com/itjiehun/nova/cube/OpenGL.java
+++ baidulbs/Nova/src/com/itjiehun/nova/cube/OpenGL.java
@@ -3,6 +3,13 @@ package com.itjiehun.nova.cube;
 import android.app.Activity;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import com.itjiehun.nova.R;
 
 public class OpenGL extends Activity {
 
@@ -11,9 +18,19 @@ public class OpenGL extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		glSurfaceView = new GLSurfaceView(this);
 		glSurfaceView.setRenderer(new CubeRender());
-		setContentView(glSurfaceView);
+		setContentView(R.layout.opengl);
+		RelativeLayout layout = (RelativeLayout) findViewById(R.id.opengl);
+		layout.addView(glSurfaceView);
+		Button back = (Button) findViewById(R.id.back_view);
+		back.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				OpenGL.this.finish();
+			}
+		});
 	}
 
 	@Override
